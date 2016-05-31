1eb7d1c8cd668b9a4f35254b9d93b4e85a936783
zy <347330613@qq.com>
2016-05-31 10:55:19 +0800

    base 64和 图片的相互转换

---
+++ src/com/imgtobase64/test/TestBase64.java
@@ -0,0 +1,12 @@
+package com.imgtobase64.test;
+
+/**
+ * 关于测试base64和图片的相互转换
+ * @author zhengYu
+ *
+ */
+public class TestBase64 {
+	public static void main(String[] args) {
+		//另外一个类已经实现了这个这些方法了，自己可以看下哈
+	}
+}
