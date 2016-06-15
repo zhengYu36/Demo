85a4cc291c5d9a0b6a4f7f6cce6fe51f9d33df2a
zy <347330613@qq.com>
2016-06-15 16:01:21 +0800

    新的测试方法

--- src/com/post/test/TestPostValiLogin.java
+++ src/com/post/test/TestPostValiLogin.java
@@ -11,7 +11,7 @@ public class TestPostValiLogin {
 	public static void main(String[] args) {
 		
 		//这个是post的方式，这个是可以这用的，这里我调取成功了
-		String sp=HttpRequest.sendPost("http://10.37.10.106:8080/scales/user/login.do", "username=tgpms1&password=tgpmssmpgt&tns=tgpmstest"); //通过get是可以把值放入进去的
+		String sp=HttpRequest.sendPost("http://10.37.10.106:8080/scales/user/login.do", "username=tgpms&password=tgpmssmpgt&tns=tgpmstest"); //通过get是可以把值放入进去的
 		System.out.println(sp);
 		//返回就 success 或 other_wrong ?如果以后这个数据有差异的话，这里应该要多有几个就可以了哈
 		
